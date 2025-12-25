be02acfc4b5d8cd095173bdad049dca242ee5c65
SanRocKk2995 <hoa74dcht21197@st.utt.edu.vn>
2025-12-25 08:52:32 +0700

    FIX REAL TIME

--- InpatientManagerSystem/InpatientManagerSystem/UI/FormWelcomePanel.cs
+++ InpatientManagerSystem/InpatientManagerSystem/UI/FormWelcomePanel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Globalization;
 
 namespace InpatientManagerSystem.UI
 {
@@ -22,25 +23,41 @@ namespace InpatientManagerSystem.UI
             timer.Tick += Timer_Tick;
             timer.Start();
 
-            // Set initial time
-            UpdateTime();
+            // Set initial time and date
+            UpdateTimeAndDate();
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            UpdateTime();
+            UpdateTimeAndDate();
         }
 
-        private void UpdateTime()
+        private void UpdateTimeAndDate()
         {
-            // Update time label with current date and time
-            lblTime.Text = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+            DateTime now = DateTime.Now;
+
+            // Update time label - only show time
+            lblTime.Text = now.ToString("HH:mm:ss");
+
+            // Update date label - show day of week and date
+            CultureInfo viCulture = new CultureInfo("vi-VN");
+            string dayOfWeek = viCulture.DateTimeFormat.GetDayName(now.DayOfWeek);
+            // Capitalize first letter
+            dayOfWeek = char.ToUpper(dayOfWeek[0]) + dayOfWeek.Substring(1);
+
+            lblDate.Text = $"{dayOfWeek}, {now:dd/MM/yyyy}";
         }
 
         private void lblTime_Click(object sender, EventArgs e)
         {
             // Optional: Click to refresh time immediately
-            UpdateTime();
+            UpdateTimeAndDate();
+        }
+
+        private void lblDate_Click(object sender, EventArgs e)
+        {
+            // Optional: Click to refresh date immediately
+            UpdateTimeAndDate();
         }
     }
 }
